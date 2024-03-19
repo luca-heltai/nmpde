a2b1a80f83604ddf0c631609aeff4152cd946dd7
Luca Heltai <luca.heltai@sissa.it>
2024-03-19 17:09:42 +0100

    Added version 0 of lab 03.

--- labs/lab-03/lab-03.cc
+++ labs/lab-03/lab-03.cc
@@ -1,9 +1,11 @@
+#include <deal.II/base/function_lib.h>
 #include <deal.II/base/point.h>
 
 #include <deal.II/dofs/dof_handler.h>
 
 #include <deal.II/fe/fe_dgq.h>
 #include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/mapping_q.h>
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
@@ -12,6 +14,7 @@
 #include <deal.II/lac/vector.h>
 
 #include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
 
 #include <iostream>
 
@@ -26,9 +29,10 @@ using namespace dealii;
  * 4. Distribute the degrees of freedom
  * 5. Interpolate a Function on the finite dimensional space
  * 6. Output the solution (using DataOut)
- * 7. Compute the error of the solution manually
+ * 7. Compute the $L^2$ error of the solution manually (looping over cells)
  * 8. Output the error
- * 9. Compute the error of the solution using VectorTools::integrate_difference
+ * 9. Compute the $L^2$ error of the solution using
+ * VectorTools::integrate_difference
  * 10. Check that the two coincide!
  * 11. Plug everything inside a function, and build a graph of the error as a
  * function of the number of degrees of freedom, refining the triangulation a
@@ -42,33 +46,82 @@ main()
   FE_Q<2>          fe(2);
   DoFHandler<2>    dof_handler(triangulation);
   GridGenerator::hyper_cube(triangulation);
-  triangulation.refine_global();
+  triangulation.refine_global(3);
 
-  // See step-2/step-3
   dof_handler.distribute_dofs(fe);
 
-  std::cout << "Number of active cells: " << triangulation.n_active_cells()
-            << std::endl
-            << "Total number of cells: " << triangulation.n_cells()
-            << std::endl;
+  // Interpolate the cosine function on this space.
+  Vector<double> solution(dof_handler.n_dofs());
+  Vector<double> solution_hand_made(dof_handler.n_dofs());
+  // Use interpolate to fill the solution vector with the values of the
+  // function at the degrees of freedom.
+  Functions::CosineFunction<2> cosine;
 
-  std::cout << "Number of degrees of freedom: " << dof_handler.n_dofs()
-            << std::endl;
+  VectorTools::interpolate(dof_handler, cosine, solution);
 
-  std::cout << "Number of dofs per cell: " << fe.dofs_per_cell << std::endl;
+  // We fill solution_hand_made with the correct values of the function.
+  auto local_support_points = fe.get_unit_support_points();
 
-  std::vector<types::global_dof_index> dofs_per_cell(fe.dofs_per_cell);
+  // Mapping from reference to real cell
+  MappingQ<2> mapping(fe.degree);
+
+  // Global numbering of degrees of freedom.
+  std::vector<types::global_dof_index> local_to_global(fe.dofs_per_cell);
+
 
   for (const auto &cell : dof_handler.active_cell_iterators())
     {
-      std::cout << cell << std::endl;
-      cell->get_dof_indices(dofs_per_cell);
-      for (const auto &dof : dofs_per_cell)
+      cell->get_dof_indices(local_to_global);
+      for (unsigned int i = 0; i < fe.dofs_per_cell; ++i)
         {
-          std::cout << dof << " ";
+          const auto global_index = local_to_global[i];
+          const auto point =
+            mapping.transform_unit_to_real_cell(cell, local_support_points[i]);
+          solution_hand_made[global_index] = cosine.value(point);
         }
-      std::cout << std::endl;
     }
+
+  // First check that we get the same result of deal.II
+  auto error = solution;
+  error -= solution_hand_made;
+  std::cout << "Error of hand made solution: " << error.l2_norm() << std::endl;
+
+  // Now we compute the L2 error
+  QGauss<2> quadrature_formula(fe.degree + 1);
+
+  // Construct a FEValues object to evaluate the shape functions and the
+  // Jacobian of the transformation from the reference cell to the real cell.
+  FEValues<2> fe_values(mapping,
+                        fe,
+                        quadrature_formula,
+                        update_values | update_quadrature_points |
+                          update_JxW_values);
+
+  double error_L2 = 0;
+
+  std::vector<double> local_values(fe_values.n_quadrature_points);
+
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      fe_values.reinit(cell);
+      double local_cell_error = 0;
+      // Computes v[q] = sum_i u_[local_to_global[i]] phi_i(q)
+      fe_values.get_function_values(solution, local_values);
+
+      for (unsigned int q = 0; q < fe_values.n_quadrature_points; ++q)
+        {
+          // compute difference of the two solutions at the quadrature point
+          // int_{T} (u_h - u)^2 dx
+          local_cell_error +=
+            (local_values[q] - cosine.value(fe_values.quadrature_point(q))) *
+            (local_values[q] - cosine.value(fe_values.quadrature_point(q))) *
+            fe_values.JxW(q);
+        }
+      error_L2 += local_cell_error;
+    }
+  error_L2 = std::sqrt(error_L2);
+  std::cout << "Ndofs:  " << dof_handler.n_dofs() << std::endl;
+  std::cout << "L2 interpolation error " << error_L2 << std::endl;
 
   DataOut<2> data_out;
 
@@ -77,16 +130,7 @@ main()
   data_out.set_flags(flags);
 
   data_out.attach_dof_handler(dof_handler);
-
-  std::vector<Vector<double>> basis_functions(
-    10, Vector<double>(dof_handler.n_dofs()));
-  for (unsigned int i = 0; i < 10; ++i)
-    {
-      Vector<double> &basis_function = basis_functions[i];
-      basis_function[i]              = 1;
-      data_out.add_data_vector(basis_function,
-                               "basis_function_" + std::to_string(i));
-    }
+  data_out.add_data_vector(solution, "solution");
   data_out.build_patches(fe.degree);
   std::ofstream output("solution.vtu");
   data_out.write_vtu(output);
