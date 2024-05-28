e88278c573dbe6ac0ddc404343623357025c14c1
Luca Heltai <luca.heltai@sissa.it>
2024-05-28 17:06:16 +0200

    include.

--- labs/lab-09/lab-09.cc
+++ labs/lab-09/lab-09.cc
@@ -9,6 +9,7 @@
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
 
+#include <deal.II/fe/fe_dgp.h>
 #include <deal.II/fe/fe_dgq.h>
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_system.h>
